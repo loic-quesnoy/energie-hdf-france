3d170687d136c1b81dec36fa013cde027266c66e
loic-quesnoy <loic@loicquesnoy.com>
2026-06-11 10:00:10 +0200

    added load function and main function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from loguru import logger
 import sys
+import datetime
+
 
 from src.extract import extract_regional_energy_data
 from src.transform import run_transformation
+from src.load import get_last_imported_date, run_loading
 
 logger.add("data/pipeline.log", rotation="10 MB", level="INFO")
 
@@ -12,13 +15,14 @@ def main():
     logger.info("=== DÉMARRAGE DU PIPELINE ÉNERGÉTIQUE REGIONAL ===")
 
     try:
-        #TODO récuperer la date du dernier enregistrement
+        last_imported_date = get_last_imported_date()
+        date_to_import = last_imported_date + datetime.timedelta(days=1)
 
-        path = extract_regional_energy_data(REGION, "2026-06-09")
+        path = extract_regional_energy_data(REGION, date_to_import.strftime("%Y-%m-%d"))
 
         run_transformation(path)
 
-        # run_loading()
+        run_loading()
 
         logger.success("=== PIPELINE EXÉCUTÉ AVEC SUCCÈS DE A À Z ===")
 
---
+++ src/load.py
@@ -0,0 +1,81 @@
+import os
+from pathlib import Path
+import polars as pl
+from sqlalchemy import create_engine,text
+from dotenv import load_dotenv
+from loguru import logger
+import datetime
+from dateutil.relativedelta import relativedelta
+
+load_dotenv()
+DATABASE_URL = f"postgresql://{os.getenv("SUPABASE_USER")}:{os.getenv("SUPABASE_PWD")}@{os.getenv("SUPABASE_HOST")}:{os.getenv("SUPABASE_PORT")}/{os.getenv("SUPABASE_DB")}"
+
+
+def run_loading():
+    logger.info("3/3 : Démarrage de la brique de Chargement (Load)...")
+
+    if not DATABASE_URL:
+        logger.critical(
+            "La variable DATABASE_URL est manquante dans le fichier .env ! "
+            "Impossible de se connecter à la base de données."
+        )
+        return
+
+    engine = create_engine(DATABASE_URL)
+    processed_dir = Path("data/processed")
+
+    tables_to_load = [
+        ("hdf_consumption.parquet", "hdf_consumption"),
+        ("hdf_production.parquet", "hdf_production")
+    ]
+
+    try:
+        for file_name, table_name in tables_to_load:
+            file_path = processed_dir / file_name
+
+            # Vérification que le fichier Parquet existe bien sur le disque
+            if not file_path.exists():
+                logger.warning(f"Le fichier {file_name} est introuvable dans {processed_dir}. Étape ignorée.")
+                continue
+
+            logger.info(f"Lecture de {file_name}...")
+            df = pl.read_parquet(file_path)
+
+            logger.info(f"Insertion de {len(df)} lignes dans la table '{table_name}'...")
+
+            # Injection magique et optimisée de Polars vers PostgreSQL
+            df.write_database(
+                table_name=table_name,
+                connection=engine,
+                if_table_exists="append"  # 'append' ajoute les lignes sans écraser la structure SQL
+            )
+            logger.success(f"Table '{table_name}' mise à jour avec succès.")
+
+        logger.success("=== LE CHARGEMENT DANS SUPABASE EST TERMINÉ===")
+
+    except Exception as e:
+        logger.error(f"Une erreur critique est survenue lors du chargement : {e}")
+        # On propage l'erreur pour que le chef d'orchestre (main.py) sache que le pipeline a échoué
+        raise e
+
+
+def get_last_imported_date() -> datetime.date:
+    """Interroge la base de données pour trouver la date la plus récente."""
+    engine = create_engine(DATABASE_URL)
+
+    # On utilise un bloc try/except au cas où la table n'existe pas encore (premier lancement)
+    try:
+        with engine.connect() as connection:
+            # Requête SQL pour choper la date maximale
+            query = text("SELECT MAX(datetime) AT TIME ZONE 'Europe/Paris' FROM hdf_consumption")
+            result = connection.execute(query).scalar()
+
+            if result is not None:
+                logger.info(f"Dernière date trouvée en base de données : {result}")
+                return result
+            else:
+                logger.warning("La table existe mais elle est vide (Première insertion).")
+                return (datetime.date.today() - relativedelta(months=1)).replace(day=1) - datetime.timedelta(days=1)
+
+    except Exception as e:
+        logger.warning(f"Impossible de lire la dernière date : {e}")
